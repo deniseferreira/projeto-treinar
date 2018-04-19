2328961dcc963c009f6f56b489408d1bc55770ab
Denise <ferreiras.denise@hotmail.com>
2018-04-19 10:19:40 +0100

    criação dos components

---
+++ loja/src/app/produto/produto.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto',
+  templateUrl: './produto.component.html',
+  styleUrls: ['./produto.component.css']
+})
+
+
+export class ProdutoComponent implements OnInit {
+
+  mostrarRes: boolean = true;
+  cards: string[] = ["Card 1", "Card 2", "Card 3"];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public mostrarResultado(){
+    this.mostrarRes = false;
+  }
+
+  public voltarResultado(){
+    this.mostrarRes = true;
+  }
+
+}
